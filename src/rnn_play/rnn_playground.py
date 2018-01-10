4e7562530ccd161c26b183fc28b9508beac36328
Malcolm Greaves <greaves.malcolm@gmail.com>
2018-01-09 23:05:10 -0800

    TODO for next steps (use LSTM)

--- src/rnn_play/rnn_playground.py
+++ src/rnn_play/rnn_playground.py
@@ -1,7 +1,24 @@
 from sys import argv
 
 def main():
-    print("Hello world! Welcome to rnn_playground !!")
+    print("""TODO
+    
+    (1) number sequence prediction
+        (1.1) (a) function to generate values 
+              (b) generate sequence
+              (c) train to memorize
+        (1.2) (a) generate _many_ sequences
+              (b) train over _all_
+              (c) predict (*should be worse....)
+        (1.3) do (1.2) but with _same family of eqns_ .e.g all linear, polynomial, etc.
+
+    (2) simple question-to-answer LSTM embedding similarity learning
+        (a) create a really simple Q/A dataset of 10 things, using _simple_ vocab
+        (b) bi-LSTM to encode question, same to encode answer
+        (c) training: encode Q,A and make sure they have similiar represnetations
+        (d) prediction: for a given Q and set of known answers, retrieve the correct one
+    
+    """)
 
 if __name__ == "__main__":
     main()
